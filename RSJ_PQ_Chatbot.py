7fc21cd7f0a93dca8bb8f06e7ea076970e6ec86a
rsjenwar <140939573+rsjenwar@users.noreply.github.com>
2025-06-24 07:27:31 +0530

    Update RSJ_PQ_Chatbot.py

--- RSJ_PQ_Chatbot.py
+++ RSJ_PQ_Chatbot.py
@@ -225,11 +225,11 @@ def submit_feedback(feedback):
 
 
 async def create_persist_db(docs, embedding, dir):
-    vector_dB = Chroma.from_documents(documents=docs,
-                                           embedding=embedding,
-                                           persist_directory=dir)
+    vector_dB = await Chroma.from_documents(documents=docs,
+                                           embedding=embedding)
+                                          # persist_directory=dir)
     # time.sleep(15)
-    vector_dB.persist()
+    # vector_dB.persist()
     return vector_dB
 
 
@@ -258,12 +258,12 @@ def RSJ_PQ_Chatbot():
             vectordb = create_persist_db(all_splits,
                                          HuggingFaceEmbeddings(),
                                          MD5hash_pdf_persits_dir)
-            vectordb = None
+            # vectordb = None
             # time.sleep(15)
             logging.debug("[RSJ | DEBUG] Created Document folder Vector Store %s %s",
                           MD5hash_pdf_persits_dir, pdf_folder_path)
-            vectordb = Chroma(persist_directory=MD5hash_pdf_persits_dir,
-                                 embedding_function=HuggingFaceEmbeddings())
+            #vectordb = Chroma(persist_directory=MD5hash_pdf_persits_dir,
+            #                     embedding_function=HuggingFaceEmbeddings())
             logging.debug("[RSJ | DEBUG] Loaded Vector Store from persistent directory %s", MD5hash_pdf_persits_dir)
         # assign in session state in first go itself
         st.session_state.vector_db = vectordb
